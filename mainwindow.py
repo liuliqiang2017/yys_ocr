18bf632e53668dd55b1e7e162eda66e188c348ff
liuliqiang <wrthew@163.com>
2018-08-29 19:36:41 +0800

    去掉名字

--- mainwindow.py
+++ mainwindow.py
@@ -155,5 +155,5 @@ class Ui_yuhun_ocr(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
-        self.label_3.setText(_translate("yuhun_ocr", "By-火电太热"))
+        self.label_3.setText(_translate("yuhun_ocr", ""))
 
